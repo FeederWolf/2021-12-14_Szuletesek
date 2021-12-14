0990ff2036abdc13f16a258b3b6ea60c47541d63
FeederWolf <Kollarg22@gmail.com>
2021-12-14 10:18:33 +0100

    Add project files.

---
+++ 2021-12-14_Születések/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_12_14_Születések
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
