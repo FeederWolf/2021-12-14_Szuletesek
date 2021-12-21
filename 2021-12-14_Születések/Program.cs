6feb63905eb66fbb7fea1e1ba736d0582928d37e
FeederWolf <Kollarg22@gmail.com>
2021-12-21 09:12:38 +0100

    9. feladat

--- 2021-12-14_Születések/Program.cs
+++ 2021-12-14_Születések/Program.cs
@@ -72,17 +72,58 @@ namespace _2021_12_14_Születések
                 if (akt > maxÉv) maxÉv = akt;
             }
             */
-            //7.b
+            //7.b SQL BE LIKE:
             int minÉv = személyek.Min(x => x.SzületésiDátum.Year);
             int maxÉv = személyek.Max(x => x.SzületésiDátum.Year);
 
             Console.WriteLine($"7. feladat: Vizsgált időszak: {minÉv} - {maxÉv}");
 
-            //.8 döntse el
+            //8. döntse el
             DateTime d = new DateTime(2004, 01, 01);
             DateTime.IsLeapYear(2004);
-            Console.WriteLine($"{személyek[0].SzületésiDátum}");
+            Console.WriteLine($"{személyek[0].SzületésiDátum}"); 
+            if (személyek.Any(x => DateTime.IsLeapYear(x.SzületésiDátum.Year) && x.SzületésiDátum.Month == 2 && x.SzületésiDátum.Day == 24))
+            {
+                Console.WriteLine("8. feladat: született");
+            }
+            else
+            {
+                Console.WriteLine("8. feladat: bruh");
+            }
+            //9. SQL BE LIKE:
+            //i got one question for you
+            /*
+            Dictionary<string, int> statisztika = new Dictionary<string, int>();
+            statisztika.Add("Dombóvár", 10011101);
+            statisztika.Add("Dick", 10011100);
+            statisztika.ContainsKey("Kapospula");
+            */
+            /*
+            Console.WriteLine("9. feladat: statisztika");
+            Dictionary<int, int> statisztika = new Dictionary<int, int>();
+            foreach (var sz in személyek)
+            {
+                if (statisztika.ContainsKey(sz.SzületésiDátum.Year))
+                {
+                    statisztika[sz.SzületésiDátum.Year]++;
+                }
+                else
+                {
+                    statisztika.Add(sz.SzületésiDátum.Year, 1);
+                }
+            }
+            foreach (var s in statisztika)
+            {
+                Console.WriteLine($"\t{s.Key} - {s.Value} fő");
+            }
+            */
 
+            //9.b SQL BE LIKE:
+            személyek
+            .GroupBy(sz => sz.születésiDátum.Year) //évenként csoportosítsa
+            .select(x => new { Évszám = x.Key, Darab = x.Count() }) //anonimuos object
+            .ToList()
+            .ForEach(y => Console.WriteLine($"\t{y.Évszám} - {y.Darab} fő"));
 
             Console.ReadKey();
         }
